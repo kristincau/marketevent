4e6071712fecc63d5eb0d7d9e3b514b868232414
Kristin Au <kristincau@Kristins-MacBook-Pro.local>
2014-07-31 18:31:39 +0900

    merge with android

---
+++ src/com/example/disneyweeklyvote/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.disneyweeklyvote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+	
+}
